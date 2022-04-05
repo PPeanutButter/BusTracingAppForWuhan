be7ed65aebe53ab52c830dd57ecf97e8e6348d98
panrunqiu <panrunqiu@outlook.com>
2022-04-05 13:35:20 +0800

    支持 竖屏

--- app/src/main/java/com/peanut/gd/wuhanbus/DetailActivity.kt
+++ app/src/main/java/com/peanut/gd/wuhanbus/DetailActivity.kt
@@ -7,6 +7,7 @@ import android.content.Context
 import android.content.Intent
 import android.content.pm.ShortcutInfo
 import android.content.pm.ShortcutManager
+import android.content.res.Configuration
 import android.graphics.Color
 import android.graphics.drawable.Icon
 import android.net.Uri
@@ -43,11 +44,14 @@ class DetailActivity : AppCompatActivity() {
             View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
         val lineId = intent.getStringExtra("lineId") ?: ""
         traceId = intent.getStringExtra("traceId") ?: ""
-        loadLine(lineId)
+        buildLine(lineId)
     }
 
+    /**
+     * 构建线路图
+     */
     @SuppressLint("UseCompatLoadingForDrawables")
-    private fun loadLine(lineId: String = line) {
+    private fun buildLine(lineId: String = line) {
         val route = binding.route
         line = lineId
         doSearch(lineId = lineId) {
@@ -61,12 +65,12 @@ class DetailActivity : AppCompatActivity() {
                     data.getString("lastTime")
                 )
                 binding.refresh.setOnClickListener {
-                    loadLine(lineId)
+                    buildLine(lineId)
                 }
                 binding.switchLine.setOnClickListener {
                     val line2Id = data.getString("line2Id")
                     if (line2Id.isNotEmpty())
-                        loadLine(line2Id)
+                        buildLine(line2Id)
                     else Toast.makeText(this, "该线路无返程哦", Toast.LENGTH_SHORT).show()
                 }
                 binding.createShortCut.setOnClickListener {
@@ -84,6 +88,9 @@ class DetailActivity : AppCompatActivity() {
                 val lines = data.getJSONArray("stops")
                 val buses = data.getJSONArray("buses")
                 route.removeAllViews()
+                /**
+                 * 生成数据缓存
+                 */
                 val state = mutableListOf<Pair<Int, Boolean>>()
                 val busid = mutableListOf<String>()
                 for (idx in 0 until buses.length()) {
@@ -92,6 +99,9 @@ class DetailActivity : AppCompatActivity() {
                     busid.add(station[0])
                 }
                 val startedStation = SettingManager.map(key = "StartedStation")
+                /**
+                 * 开始渲染车站
+                 */
                 for (idx in 0 until lines.length()) {
                     val station = lines.getJSONObject(idx)
                     val stopName = station.getString("stopName")
@@ -99,23 +109,39 @@ class DetailActivity : AppCompatActivity() {
                     val item = buildRouteItem(stopName, stopId, stopId in startedStation)
                     route.addView(item)
                 }
+                /**
+                 * 检查是否有需要跟踪的公交，有的话做高亮处理
+                 */
                 val traceIndex = busid.indexOf(traceId)
+                /**
+                 * 判断车站是否有车，车是否到站，到站了需要修改车图标的布局。
+                 */
                 for ((i,bus) in state.withIndex()) {
                     val view = route.getChildAt(bus.first - 1)
                     val busIcon = view.findViewById<ImageView>(R.id.imageView4)
                     busIcon.visibility = View.VISIBLE
                     if (bus.second.not()) {
                         busIcon.setImageDrawable(resources.getDrawable(R.drawable.ic_round_directions_bus_24))
-                        val a = busIcon.layoutParams as ConstraintLayout.LayoutParams
-                        a.startToStart = 0
-                        a.endToEnd = -1
-                        busIcon.layoutParams = a
+                        (busIcon.layoutParams as ConstraintLayout.LayoutParams).let { layoutParams->
+                            if(this.resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+                                layoutParams.startToStart = 0
+                                layoutParams.endToEnd = -1
+                            }else{
+                                layoutParams.topToTop = 0
+                                layoutParams.bottomToBottom = -1
+                            }
+                            busIcon.layoutParams = layoutParams
+                        }
                     }
+                    //高亮处理
                     if (i == traceIndex){
                         //set background
                         busIcon.setBackgroundResource(R.drawable.traced_bus)
                         busIcon.setImageDrawable(resources.getDrawable(R.drawable.ic_round_directions_bus_fff_24))
                     }
+                    /**
+                     * 添加分享当前所在车。
+                     */
                     busIcon.setOnClickListener {
                         try {
                             ("点击查看我的位置 "+Uri.parse("https://peanutbutter.gitee.io/exercise.share?").buildUpon()
@@ -130,17 +156,31 @@ class DetailActivity : AppCompatActivity() {
                         }
                     }
                 }
+                /**
+                 * 第一个车站，前面的线段需要隐藏
+                 */
                 route.getChildAt(0).apply {
-                    val line = this.findViewById<ImageView>(R.id.imageView2)
-                    val a = line.layoutParams as ConstraintLayout.LayoutParams
-                    a.startToStart = R.id.imageView3
-                    line.layoutParams = a
+                    this.findViewById<ImageView>(R.id.imageView2).apply {
+                        (this.layoutParams as ConstraintLayout.LayoutParams).let { layoutParams ->
+                            if(this.resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE)
+                                layoutParams.startToStart = R.id.imageView3
+                            else layoutParams.topToTop = R.id.imageView3
+                            this.layoutParams = layoutParams
+                        }
+                    }
                 }
+                /**
+                 * 最后一个车站，后面的线段需要隐藏
+                 */
                 route.getChildAt(route.childCount - 1).apply {
-                    val line = this.findViewById<ImageView>(R.id.imageView2)
-                    val a = line.layoutParams as ConstraintLayout.LayoutParams
-                    a.endToEnd = R.id.imageView3
-                    line.layoutParams = a
+                    this.findViewById<ImageView>(R.id.imageView2).apply {
+                        (this.layoutParams as ConstraintLayout.LayoutParams).let { layoutParams ->
+                            if(this.resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE)
+                                layoutParams.endToEnd = R.id.imageView3
+                            else layoutParams.bottomToBottom = R.id.imageView3
+                            this.layoutParams = layoutParams
+                        }
+                    }
                 }
             } catch (e: Exception) {
                 e.printStackTrace()
@@ -148,6 +188,9 @@ class DetailActivity : AppCompatActivity() {
         }
     }
 
+    /**
+     * 添加桌面快捷方式，仅支持Android O +
+     */
     @RequiresApi(Build.VERSION_CODES.O)
     fun addShortCut(context: Context, busId: String, label: String) {
         val shortcutManager = context.getSystemService(Context.SHORTCUT_SERVICE) as ShortcutManager
@@ -164,6 +207,9 @@ class DetailActivity : AppCompatActivity() {
         }
     }
 
+    /**
+     * 执行搜索，包括手动与自动
+     */
     private fun doSearch(lineId: String, func: (JSONObject) -> Unit) {
         var retry = 5
         var success = false
@@ -208,10 +254,13 @@ class DetailActivity : AppCompatActivity() {
         }
     }
 
+    /**
+     * 渲染车站视图,支持收藏
+     */
     private fun buildRouteItem(stationName: String, stopId: String, started: Boolean): View {
         val view = this.layoutInflater.inflate(R.layout.station, null)
         view.findViewById<TextView>(R.id.station_name).apply {
-            this.text = ("$stationName${if (started) "★" else ""}").toCharArray().joinToString("\n")
+            this.text = ("$stationName${if (started) "★" else ""}").toCharArray().joinToString(if(this.resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE) "\n" else "")
             if (started)
                 this.setTextColor(resources.getColor(R.color.primary))
             this.setOnClickListener {
@@ -220,11 +269,11 @@ class DetailActivity : AppCompatActivity() {
                     message(text = "收藏后,${stationName}将高亮显示以方便您查找")
                     positiveButton(text = "收藏") {
                         SettingManager.plus(key = "StartedStation", stopId)
-                        loadLine()
+                        buildLine()
                     }
                     negativeButton(text = "移除") {
                         SettingManager.remove(key = "StartedStation", stopId)
-                        loadLine()
+                        buildLine()
                     }
                 }
             }
@@ -232,6 +281,9 @@ class DetailActivity : AppCompatActivity() {
         return view
     }
 
+    /**
+     * 复制文字到剪切板
+     */
     private fun String.copy(context: Context) {
         val clipboard: ClipboardManager? =
             context.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager?
